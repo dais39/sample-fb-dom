b87361a9559b83954a08b82630a05bd2bb07cea7
Daisaku Suzuki <ds.suzuki217@gmail.com>
2019-12-11 22:49:27 +0900

    facebookの投稿テキストを決まった文言に変更する処理を追加

--- hide-user-content.js
+++ hide-user-content.js
@@ -1,31 +1,40 @@
 const hide_user_content = () => {
     const elementList = document.getElementsByClassName('userContent');
 
-    // 投稿されたテキストを全て非表示にする
-    // while (elements.length !== 0) {
-    //     elements[0].parentNode.removeChild(elements[0]);
-    // }
-
-    // 投稿されたテキストを取得してコンソールに表示する
-    let text = '';
+    // 投稿されたテキストを定型文に差し替える
     for (el of elementList) {
         const pList = el.getElementsByTagName('p');
-        for (p of pList) {
-            text += p.innerText;
+
+        // 投稿テキストを取得
+        let text = ''
+        if (pList.length !== 0){
+            for (p of pList) {
+                text += pList[0].innerText;
+            }
         }
-        console.log(text);
-        text = '';
+        console.log(text)
+
+        // userContent配下の要素を全消し
+        let child = el.lastChild;
+        while(child){
+            el.removeChild(child);
+            child = el.lastChild;
+        }
+
+        const newEl = document.createElement('p');
+        newEl.innerText = 'Hello World!';
+        el.appendChild(newEl);
+        // userContentの子要素としてpタグを追加
     }
 }
 
 const observer = new MutationObserver(hide_user_content);
-const target = document.querySelector('[role="feed"]').childNodes[2];
 
-console.log(target);
+// facebookのfeedは<div role=feed>の子要素<div id=more_pager_pagelet_xxxxx>の孫要素が増えていくので、<div id=more_pager_pagelet_xxxxx>の子要素を監視対象にする
+const target = document.querySelector('[role="feed"]').childNodes[2].firstChild;
 
 observer.observe(target, {
     childList: true,
-    subtree: true
 });
 
 hide_user_content();
