02f9fafbc64323581b85f8586fd128fca019d1f7
Daisaku Suzuki <ds.suzuki217@gmail.com>
2019-12-14 18:07:56 +0900

    jqueryを使った同期APIリクエストを実装

--- background.js
+++ background.js
@@ -1,16 +1,26 @@
 chrome.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
-        const xhr = new XMLHttpRequest();
-        const baseUrl = "http://localhost:8080/transform";
-        const requestBody = {
-            "text": request.text
-        };
+        // const xhr = new XMLHttpRequest();
+        // const baseUrl = "http://localhost:8080/transform";
+        // const requestBody = {
+        //     "text": request.text
+        // };
 
-        xhr.open("POST", baseUrl, false);
-        xhr.setRequestHeader("Content-type", "application/json");
-        const translatedText = xhr.send(JSON.stringify(requestBody));
+        // xhr.open("POST", baseUrl, false);
+        // xhr.setRequestHeader("Content-type", "application/json");
+        // const translatedText = xhr.send(JSON.stringify(requestBody));
 
-        sendResponse({ text: translatedText });
-
-        return true;
-    });
+        $.ajax({
+            url: "http://localhost:8080/transform",
+            type: "POST",
+            async: false,
+            contentType: "application/json",
+            data: JSON.stringify({
+                "text": request.text
+            }),
+            success: function (res) {
+                sendResponse({ text: res.text });
+            }
+        })
+    }
+);
